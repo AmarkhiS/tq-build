57c90cce6ce26678eaac89f37de14a81b9350d70
AmarkhiS <422229+AmarkhiS@users.noreply.github.com>
2022-04-22 16:01:33 +0200

    Display classes with two masteries

--- js/index.js
+++ js/index.js
@@ -46,21 +46,25 @@ var decode_html_entities = function ( value ) {
 
 
 /**
- * Display all mono classes
+ * Parse classes list to a clean ordered display
+ *
+ * @return {dict} Classes
  */
-var display_classes_base = function () {
-    /**
-     * Element to display all classes
-     * @type {dom}
-     */
-    let el_div = $( 'div#classes-base' );
-
+var parse_classes_base = function () {
     /**
      * Classes
-     * @type {str[]}
+     * @type {dict}
+     */
+    let ret = {};
+    
+    /**
+     * Classes sorted by name
+     * @type {string[]}
      */
     let tq_classes = Object.keys ( classes [ 'base' ] );
-    tq_classes.sort ();
+    tq_classes.sort ( function ( a, b ) {
+        return a.localeCompare ( b );
+    } );
     
     tq_classes.forEach ( function ( tq_class ) {
         /**
@@ -86,7 +90,10 @@ var display_classes_base = function () {
             `./img/global/logo-${mastery}.png`
         ).attr (
             'alt',
-            decode_html_entities ( class_name )
+            decode_html_entities ( mastery_name [ mastery ] )
+        ).attr (
+            'title',
+            decode_html_entities ( mastery_name [ mastery ] )
         );
 
         /**
@@ -104,10 +111,163 @@ var display_classes_base = function () {
             el_img
         );
 
+        ret [ class_name ] = el_name;
+    } );
+    
+    return ret;
+};
+
+
+/**
+ * Display all mono classes
+ */
+var display_classes_base = function () {
+    /**
+     * Element to display all classes
+     * @type {dom}
+     */
+    let el_div = $( 'div#classes-base' );
+    
+    /**
+     * Datas to display
+     */
+    let datas = parse_classes_base ();
+    
+    /**
+     * Classes
+     * @type {str[]}
+     */
+    let tq_classes = Object.keys ( datas );
+    tq_classes.sort ( function ( a, b ) {
+        return a.localeCompare ( b );
+    } );
+    
+    tq_classes.forEach ( function ( tq_class ) {
         el_div.append (
-            $( '<div />' ).append (
-                el_name
-            )
+            $( '<div />' ).append ( datas [ tq_class ] )
+        );
+    } );
+};
+
+
+/**
+ * Parse classes list to a clean ordered display
+ *
+ * @return {dict} Classes
+ */
+var parse_classes_combo = function () {
+    /**
+     * Classes
+     * @type {dict}
+     */
+    let ret = {};
+
+    /**
+     * Ordered classes
+     * @type {string[]}
+     */
+    let tq_classes = Object.keys ( classes [ 'combo' ] );
+    tq_classes.sort ( function ( a, b ) {
+        return a.localeCompare ( b );
+    } );
+    
+    tq_classes.forEach ( function ( tq_class ) {
+        /**
+         * Masteries of current class
+         * @type {str}
+         */
+        let masteries = [
+            classes [ 'combo' ] [ tq_class ] [ 'mastery_1' ],
+            classes [ 'combo' ] [ tq_class ] [ 'mastery_2' ]
+        ];
+
+        /**
+         * Masteries on french version
+         * @type {dict}
+         */
+        let masteries_fr = {};
+        masteries.forEach ( function ( mastery ) {
+            masteries_fr [ mastery_name [ mastery ] ] = mastery;
+        } );
+        
+        masteries = Object.keys ( masteries_fr );
+        masteries.sort ( function ( a, b ) {
+            return b.localeCompare ( a );
+        } );
+        
+        /**
+         * Current class name
+         * @type {str}
+         */
+        let class_name = classes [ 'combo' ] [ tq_class ] [ 'name' ];
+        
+        /**
+         * Class name : link with image & name
+         * @type {dom}
+         */
+        let el_name = $(
+            '<a />'
+        ).attr (
+            'href',
+            'http://www.google.fr'
+        ).html (
+            class_name
+        );
+        
+        masteries.forEach ( function ( mastery ) {
+            mastery = masteries_fr [ mastery ];
+            
+            /**
+             * Class image
+             * @type {dom}
+             */
+            let el_img = $(
+                '<img />'
+            ).attr (
+                'src',
+                `./img/global/logo-${mastery}.png`
+            ).attr (
+                'alt',
+                decode_html_entities ( mastery_name [ mastery ] )
+            ).attr (
+                'title',
+                decode_html_entities ( mastery_name [ mastery ] )
+            );
+            
+            el_name.prepend (
+                el_img
+            );
+        } );
+
+        ret [ class_name ] = el_name;
+    } );
+    
+    return ret;
+};
+
+
+/**
+ * Display all mono classes
+ */
+var display_classes_combo = function () {
+    /**
+     * Element to display all classes
+     * @type {dom}
+     */
+    let el_div = $( 'div#classes-combo' );
+    
+    let datas = parse_classes_combo ();
+    
+    /**
+     * Classes
+     * @type {str[]}
+     */
+    let tq_classes = Object.keys ( datas );
+    tq_classes.sort ();
+    
+    tq_classes.forEach ( function ( tq_class ) {
+        el_div.append (
+            $( '<div />' ).append ( datas [ tq_class ] )
         );
     } );
 };
@@ -119,6 +279,9 @@ var display_classes_base = function () {
 var display_classes = function () {
     // Classes : mono
     display_classes_base ();
+    
+    // Classes : duo
+    display_classes_combo ();
 };
 
 
